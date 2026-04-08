af197bf8fe43d7b9c5e29347db9e432d823ad812
Woodrow Bell <wmn1038@gmail.com>
2026-04-07 15:53:04 -1000

    Initial Commit

---
+++ Survival Guide/Survival_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Survival_GuideApp.swift
+//  Survival Guide
+//
+//  Created by Woodrow Bell on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Survival_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
